d2e37d9ca3f8f1729fedee37d83412bae847cf7e
sshwy <jy.cat@qq.com>
2025-12-31 22:44:19 +0800

    feat: beautify fold icons

--- src/lib/editor-view.ts
+++ src/lib/editor-view.ts
@@ -283,7 +283,20 @@ export function useEditorView(el: Element, init: InitOptions) {
   const staticExtensions = {
     common: [
       lineNumbers({}),
-      foldGutter(),
+      foldGutter({
+        markerDOM(open) {
+          const div = document.createElement("div");
+          if (!open) {
+            div.innerHTML =
+              '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>';
+          } else {
+            div.innerHTML =
+              '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>';
+          }
+          div.className = "flex items-center justify-center h-full";
+          return div;
+        },
+      }),
       commentFoldService,
       gutter({ class: "cm-gutters" }),
       bracketMatching(),
