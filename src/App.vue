e458e60607bc6cefee7731e3157f7126a90207ba
sshwy <jy.cat@qq.com>
2025-12-31 23:14:17 +0800

    feat: set default color mode to 'auto' and update component descriptions to English

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { useColorMode } from '@vueuse/core'
 import { computed, ref } from 'vue';
 
 const color = useColorMode()
+color.value = 'auto'
 const presentColor = computed(() => color.value as 'light' | 'dark')
 
 const items = [{
@@ -36,11 +37,11 @@ const code = ref(euclideanoidText)
       <h1 class="text-2xl font-medium my-4">Sshwy's OI Code Editor</h1>
 
       <p>
-        OICodeEditor 包含一系列基于 codemirror6 和
-        Vue3，支持多文件、语法高亮和主题切换的代码查看或编辑组件。
+        OICodeEditor contains a set of code viewing or editing components based on codemirror6
+        and Vue3, supporting multi-file, syntax highlighting and theme switching.
       </p>
 
-      <p>简单代码编辑器示例：</p>
+      <p>Simple code editor demo:</p>
 
       <CodeEditor
         class="border border-slate-300 dark:border-slate-700"
@@ -49,7 +50,7 @@ const code = ref(euclideanoidText)
         lang="cpp"
       />
 
-      <p>静态代码展示示例：</p>
+      <p>Static code viewing demo:</p>
 
       <CodeViewer
         :items="items"
