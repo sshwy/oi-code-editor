0fb6c398ec424c560a5ddd8005158c0b75285cc8
sshwy <jy.cat@qq.com>
2025-12-31 17:11:53 +0800

    chore: update package configuration and enhance type definitions

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,10 @@
+import type { DefineComponent } from 'vue'
 import HelloWorld from './HelloWorld.vue'
 
 export { HelloWorld }
+
+type PropsOf<C> = C extends DefineComponent<infer P, any, any, any, any, any, any, any> ? P : never
+
+export type HelloWorldComponent = typeof HelloWorld
+export type HelloWorldProps = PropsOf<HelloWorldComponent>
 
