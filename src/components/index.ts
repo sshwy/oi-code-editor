bb5b12ddbae2e475c3f281e2756b9251ce8bf67e
sshwy <jy.cat@qq.com>
2026-01-02 21:41:12 +0800

    fix: fix props type of components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,9 @@
-import type { DefineComponent } from "vue";
 import CodeViewer from "./CodeViewer.vue";
 import CodeEditor from "./CodeEditor.vue";
 
 export { CodeViewer, CodeEditor };
 
-type PropsOf<C> = C extends DefineComponent<infer P, any, any, any, any, any, any, any> ? P : never;
+type PropsOf<C> = C extends new (...args: any[]) => { $props: infer P } ? P : never;
 
 export type CodeViewerComponent = typeof CodeViewer;
 export type CodeViewerProps = PropsOf<CodeViewerComponent>;
