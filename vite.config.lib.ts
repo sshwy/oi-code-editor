b93d46a18e4c8db09fbe3312b48a83f0a53a779f
sshwy <jy.cat@qq.com>
2026-01-02 11:31:05 +0800

    chore: reorganize dependencies in package.json and pnpm-lock.yaml, and add CodeMirror globals in vite.config.lib.ts

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -2,6 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
+const codemirrorGlobals = {
+  "@codemirror/commands": "CodeMirrorCommands",
+  "@codemirror/lang-cpp": "CodeMirrorLangCpp",
+  "@codemirror/lang-json": "CodeMirrorLangJson",
+  "@codemirror/lang-markdown": "CodeMirrorLangMarkdown",
+  "@codemirror/lang-rust": "CodeMirrorLangRust",
+  "@codemirror/language": "CodeMirrorLanguage",
+  "@codemirror/merge": "CodeMirrorMerge",
+  "@codemirror/state": "CodeMirrorState",
+  "@codemirror/theme-one-dark": "CodeMirrorThemeOneDark",
+  "@codemirror/view": "CodeMirrorView",
+};
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -17,10 +30,11 @@ export default defineConfig({
       fileName: "oi-code-editor",
     },
     rollupOptions: {
-      external: ["vue"],
+      external: ["vue", ...Object.keys(codemirrorGlobals)],
       output: {
         globals: {
           vue: "Vue",
+          ...codemirrorGlobals,
         },
       },
     },
