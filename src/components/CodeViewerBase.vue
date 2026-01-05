e2d8aff9a4c3447273074f6c1c50ad636499ebf9
sshwy <jy.cat@qq.com>
2026-01-05 22:26:17 +0800

    refactor: extract tab management into a new TabsPanel component for improved code organization and reusability

--- src/components/CodeViewerBase.vue
+++ src/components/CodeViewerBase.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref, useTemplateRef, watch } from 'vue';
 import { type I18nPrases, useEditorView, type EditorInstance, type FoldOptions, type InitOptions, type LangKind, type TabItem, type EditMode, type ColorMode } from '~/lib';
+import TabsPanel from './TabsPanel.vue';
 
 const props = defineProps<{
   content?: string;
@@ -107,60 +108,7 @@ onMounted(() => {
 
 <template>
   <div>
-    <div v-if="tabs" class="code-viewer-tab-panel">
-      <div
-        v-for="item in tabs"
-        :key="item.id"
-        :class="['code-viewer-tab-item', activeTab === item.id ? 'code-viewer-tab-item-active' : '']"
-        @click="activeTab = item.id"
-      >
-        {{ item.label }}
-      </div>
-    </div>
+    <TabsPanel v-if="tabs" :tabs="tabs" v-model="activeTab" />
     <div ref="editorRoot" class="code-viewer-base" />
   </div>
 </template>
-
-<style>
-.code-viewer-tab-panel {
-  position: sticky;
-  top: var(--oce-header-height);
-  z-index: 10;
-  display: flex;
-  font-size: 13px;
-  border: 1px solid #ddd;
-  border-bottom: none;
-  background-color: #f5f5f5;
-  color: black;
-}
-.dark .code-viewer-tab-panel {
-  border: none;
-  background-color: #21252b;
-  color: #abb2bf;
-}
-
-.code-viewer-tab-panel > .code-viewer-tab-item {
-  padding: 4px 12px;
-  border-right: 1px solid #ddd;
-  cursor: pointer;
-}
-.dark .code-viewer-tab-panel > .code-viewer-tab-item {
-  border-right: 1px solid #383838;
-}
-
-.code-viewer-tab-panel > .code-viewer-tab-item:hover {
-  background-color: #ffffff80;
-}
-.dark .code-viewer-tab-panel > .code-viewer-tab-item:hover {
-  background-color: #ffffff0f;
-}
-
-.code-viewer-tab-item-active {
-  background-color: #fff;
-  color: black;
-}
-.dark .code-viewer-tab-item-active {
-  background-color: #282c34;
-  color: #eee;
-}
-</style>
---
+++ src/components/TabsPanel.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import type { TabItem } from '~/lib';
+
+defineProps<{
+  tabs: TabItem[];
+}>();
+
+const activeTab = defineModel<string | undefined>({ required: true });
+</script>
+<template>
+  <div class="code-viewer-tab-panel">
+    <div
+      v-for="item in tabs"
+      :key="item.id"
+      :class="['code-viewer-tab-item', activeTab === item.id ? 'code-viewer-tab-item-active' : '']"
+      @click="activeTab = item.id"
+    >
+      {{ item.label }}
+    </div>
+  </div>
+</template>
+
+<style>
+.code-viewer-tab-panel {
+  position: sticky;
+  top: var(--oce-header-height);
+  z-index: 10;
+  display: flex;
+  font-size: 13px;
+  border: 1px solid #ddd;
+  border-bottom: none;
+  background-color: #f5f5f5;
+  color: black;
+}
+
+.dark .code-viewer-tab-panel {
+  border: none;
+  background-color: #21252b;
+  color: #abb2bf;
+}
+
+.code-viewer-tab-panel>.code-viewer-tab-item {
+  padding: 4px 12px;
+  border-right: 1px solid #ddd;
+  cursor: pointer;
+}
+
+.dark .code-viewer-tab-panel>.code-viewer-tab-item {
+  border-right: 1px solid #383838;
+}
+
+.code-viewer-tab-panel>.code-viewer-tab-item:hover {
+  background-color: #ffffff80;
+}
+
+.dark .code-viewer-tab-panel>.code-viewer-tab-item:hover {
+  background-color: #ffffff0f;
+}
+
+.code-viewer-tab-item-active {
+  background-color: #fff;
+  color: black;
+}
+
+.dark .code-viewer-tab-item-active {
+  background-color: #282c34;
+  color: #eee;
+}
+</style>
